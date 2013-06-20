8feb93c90dd6a0dfef1348e6d4621ab0790c03ed
Rachel Sanders <rachel@trustrachel.com>
2013-06-20 16:32:47 -0700

    Revving the version number for no other reason that installing via pip wasn't working. It is now?

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if "develop" in argv:
 
 setup(
   name='Flask-FeatureFlags',
-  version='0.1',
+  version='0.2',
   url='https://github.com/trustrachel/Flask-FeatureFlags',
   license='Apache',
   author='Rachel Sanders',
