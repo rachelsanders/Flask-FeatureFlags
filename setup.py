dab375ff09966463c94672d45cbd1589df8d7389
Rachel Sanders <rachel@trustrachel.com>
2015-06-09 19:20:51 -0700

    Setting to dev status

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if "develop" in argv:
 
 setup(
   name='Flask-FeatureFlags',
-  version='0.6',
+  version='0.7-dev',
   url='https://github.com/trustrachel/Flask-FeatureFlags',
   license='Apache',
   author='Rachel Sanders',
